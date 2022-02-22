950388ad6ecc89990756d4f2c1c8025cfafe60b3
jiacai2050 <dev@liujiacai.net>
2022-02-22 21:15:06 +0800

    add revert

--- onehistory.el
+++ onehistory.el
@@ -19,11 +19,6 @@
   :group 'onehistory
   :type 'file)
 
-(defcustom onehistory-latest-history-limit 1000
-  "Limit how many histories return when call query-latest"
-  :group 'onehistory
-  :type 'integer)
-
 (defcustom onehistory-eww-integration t
   "Whether save eww history to onehistory"
   :group 'onehistory
@@ -34,11 +29,25 @@
   :group 'onehistory
   :type 'boolean)
 
+(defcustom onehistory-default-query-keyword nil
+  "Default keyword for query history"
+  :group 'onehistory
+  :type '(choice (const :tag "Don't set" nil)
+                 (string :tag "Query keyword")))
+
+(defcustom onehistory-default-query-limit 1000
+  "Default limit for query history"
+  :group 'onehistory
+  :type 'number)
+
 (defvar onehistory-db nil
   "The core database for elfeed.")
 
-(defvar onehistory-search-keyword nil
+(defvar onehistory-query-keyword onehistory-default-query-keyword
   "The case-insensitive keyword used when query history.")
+
+(defvar onehistory-query-limit onehistory-default-query-limit
+  "Limit how many histories return when call query-latest")
 
 (defvar onehistory-mode-map
   (let ((map (make-sparse-keymap)))
@@ -47,6 +56,7 @@
     (define-key map (kbd "w") 'onehistory-copy-history-url)
     (define-key map (kbd "t") 'onehistory-copy-history-title)
     (define-key map (kbd "s") 'onehistory-search-by-keyword)
+    (define-key map (kbd "s-u") 'tabulated-list-revert)
     map)
   "Local keymap for onehistory mode buffers.")
 
@@ -65,13 +75,13 @@
   (onehistory-dyn--query-histories-by-range onehistory-db
                                             (string-to-number (format-time-string "%s" start-time))
                                             (string-to-number (format-time-string "%s" end-time))
-                                            onehistory-search-keyword))
+                                            onehistory-query-keyword))
 
-(defun onehistory-query-latest (&optional limit)
+(defun onehistory-query-latest ()
   (onehistory-db-ensure)
   (onehistory-dyn--query-latest-histories onehistory-db
-                                          (or limit onehistory-latest-history-limit)
-                                          onehistory-search-keyword))
+                                          onehistory-query-limit
+                                          onehistory-query-keyword))
 
 (defun onehistory-eww-hook ()
   (let ((title (plist-get eww-data :title))
@@ -146,20 +156,15 @@
         (kill-new title))
     (user-error "There is no history at point")))
 
-(defun onehistory-vec-to-list (histories)
-  (seq-into histories 'list))
-
-(defun onehistory-refresh-tabulated-list ()
-  (setq tabulated-list-entries
-        (lambda ()
-          (onehistory-vec-to-list (onehistory-query-latest))))
-  (tabulated-list-print t))
+(defun onehistory-tabulated-list-revert (&optional revert)
+  (setq-local onehistory-query-keyword onehistory-default-query-keyword)
+  (setq-local onehistory-query-limit onehistory-default-query-limit))
 
 (defun onehistory-search-by-keyword (keyword)
   (interactive "sKeyword: ")
   (when (eq major-mode 'onehistory-mode)
-    (setq-local onehistory-search-keyword keyword)
-    (onehistory-refresh-tabulated-list)))
+    (setq-local onehistory-query-keyword keyword)
+    (tabulated-list-print t)))
 
 (define-derived-mode onehistory-mode tabulated-list-mode "onehistory" "History solution for Emacs"
   (setq tabulated-list-format [("Time" 20 t)
@@ -168,9 +173,14 @@
                                ("Visit Count" 10 (lambda (x y)
                                                    (>
                                                     (string-to-number (aref (cadr x) 3))
-                                                    (string-to-number (aref (cadr y) 3)))))])
-  (setq tabulated-list-padding 2)
-  (setq tabulated-list-sort-key (cons "Time" t))
+                                                    (string-to-number (aref (cadr y) 3)))))]
+
+        tabulated-list-padding 2
+        tabulated-list-sort-key (cons "Time" t)
+        tabulated-list-entries (lambda ()
+                                 (seq-into (onehistory-query-latest) 'list)))
+
+  (add-hook 'tabulated-list-revert-hook 'onehistory-tabulated-list-revert nil t)
   (tabulated-list-init-header))
 
 ;;;###autoload
@@ -179,7 +189,7 @@
   (interactive)
   (with-current-buffer (get-buffer-create "*onehistory*")
     (onehistory-mode)
-    (onehistory-refresh-tabulated-list)
+    (tabulated-list-print t)
     (switch-to-buffer (current-buffer))))
 
 (provide 'onehistory)
