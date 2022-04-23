bc4e53634f02ccd85439cb51d2ab52a568cc0ad7
jiacai2050 <dev@liujiacai.net>
2022-04-23 21:05:56 +0800

    Add LICENSE

--- eww-history-ext.el
+++ eww-history-ext.el
@@ -8,6 +8,29 @@
 ;; Keywords: eww, elfeed, history
 ;; URL: https://github.com/1History/eww-history-ext
 
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; This file is NOT part of GNU Emacs
+
+;;; Commentary:
+;;
+;; See documentation in README.org or visit homepage
+
 ;;; Code:
 
 (require 'eww-history-ext-dyn)
